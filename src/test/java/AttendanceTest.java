b2a873c621d02fc58daab624226e33175777937a
mlle-alois <55437841+mlle-alois@users.noreply.github.com>
2021-02-24 14:12:53 +0100

    [COMMIT]

---
+++ src/test/java/AttendanceTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+public class AttendanceTest {
+  @Test
+  void test1() {
+    // TODO : The duration of an attendance starting at 14:00 and finishing at 15:30 should be 90 minutes
+  }
+
+  @Test
+  void test2() {
+    // TODO : The duration of an attendance starting at 16:00 and finishing at 15:30 should be 0 minutes
+  }
+
+  @Test
+  void test3() {
+    // TODO : If I bound an attendance from 14h30 to 15h30 with a session from 14h00 to 15h00, then I should get a new Attendance between 14h30 and 15h00
+  }
+}
